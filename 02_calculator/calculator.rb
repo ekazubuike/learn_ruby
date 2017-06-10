2336f2aa7ba07079d7d716d045beaa034d0fb3a8
EK Azubuike <ayo.azubuike@gmail.com>
2017-06-10 11:21:29 -0500

    all tests pass

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -12,17 +12,32 @@ def sum (array)
 	return total
 end
 
-def multiply(*args)
+def multiply (*args)
 	total = 1
 	args.each {|x| total *= x}
 	return total
 end
 
-def power(num, power)
+def power (num, power)
 	total = 1
 	power.times do
 		total *= num
 	end
+	return total
+end
+
+def factorial (num)
+	total = 1
+	array = []
+	if num == 0
+		return total
+	else
+		while num > 0
+			array.push(num)
+			num -= 1
+		end	
+	end
+	array.each {|x| total *= x}
 	return total
 end
 
