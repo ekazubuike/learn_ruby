2a16430a1dbbe3ab1e8f192beffc110909db4240
EK Azubuike <ayo.azubuike@gmail.com>
2017-06-09 21:48:33 -0500

    added add, subtract, sum, multiply, and power methods to calculator.rb; added multiply and power tests to _spec.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
-#write your code here
+def add (num1, num2)
+	return num1 + num2
+end
+
+def subtract (num1, num2)
+	return num1 - num2
+end
+
+def sum (array)
+	total = 0
+	array.each {|x| total += x}
+	return total
+end
+
+def multiply(*args)
+	total = 1
+	args.each {|x| total *= x}
+	return total
+end
+
+def power(num, power)
+	total = 1
+	power.times do
+		total *= num
+	end
+	return total
+end
+
