01f8f8c6afae10c32be4f830be1178fbb892b277
EK Azubuike <ayo.azubuike@gmail.com>
2017-06-12 22:50:27 -0500

    All tests pass

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
-#write your code here
+def echo phrase
+	return phrase
+end
+
+def shout phrase
+	return phrase.upcase
+end
+
+def repeat (phrase, num = 2)
+	return ([phrase] * num).join(' ')
+end
+
+def start_of_word (word, n)
+	return word[0..n-1]
+end
+
+def first_word phrase
+	return phrase.split(' ')[0]
+end
+
+LITTLE_WORDS = ["the", "over", "an", "and"]
+def titleize words
+	phrase = words.split(' ')
+	phrase.each do |word|
+		if LITTLE_WORDS.index(word) != nil && phrase.index(word) > 0
+			word
+		else
+			word.capitalize!
+		end
+	end
+	return phrase.join(' ')
+end
