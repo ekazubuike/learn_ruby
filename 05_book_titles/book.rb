5cc03d1d74dc5e57abba126b06a5802dade74c69
EK Azubuike <ayo.azubuike@gmail.com>
2017-06-17 14:18:22 -0500

    all tests pass

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
+
+
 class Book
-# write your code here
+	EXCEPTIONS = ["and", "but", "or", "in", "the", "of", "a", "an"]
+
+	attr_accessor :title
+	
+	def title= title
+		if title.length > 1
+			phrase = title.split(' ')
+			phrase.each do |word|
+				if EXCEPTIONS.index(word) != nil && phrase.index(word) > 0
+					word
+				else
+					word.capitalize!
+				end
+			end
+			@title = phrase.join(' ')
+		else
+			@title = title.capitalize
+		end
+	end
 end
