674e85787c303f5f945e5ed3a8f8f1eea214e1bc
EK Azubuike <ayo.azubuike@gmail.com>
2017-06-17 15:59:34 -0500

    all tests pass

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,31 @@
 class Timer
-  #write your code here
+	attr_accessor :time
+	def initialize
+		@time = 0
+	end
+
+	def seconds
+		@time
+	end
+
+	def seconds= (number)
+		@time = number
+	end
+
+	def time_string
+		hours = (@time - (@time%3600))/3600
+		@time = @time - (hours*3600)
+		minutes = (@time - (@time%60))/60
+		seconds = @time - (minutes*60)
+
+		return "#{plus_zero(hours)}:#{plus_zero(minutes)}:#{plus_zero(seconds)}"	
+	end
+
+	def plus_zero int
+		if int.to_s.length == 1
+			int = "0" + int.to_s
+		else
+			int = int.to_s
+		end
+	end
 end
